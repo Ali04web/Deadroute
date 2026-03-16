81cdcf20bd53b2a86da8750c33d2e010500731d9
Ali Husain <alihusain.or@gmail.com>
2026-03-16 05:35:41 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
-// app/page.tsx
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
 
-export default async function RootPage() {
-  const session = await getServerSession(authOptions);
-  if (session) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+import LandingClient from "./landing-client";
+
+export default function Page() {
+  return <LandingClient />;
 }
